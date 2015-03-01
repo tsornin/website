e43e0fcfce28438a9d338d409fd7fe4268e7694d
Bruno Li (Desktop) <bruno.sie.li@gmail.com>
2015-02-28 22:07:48 -0800

    minor tweaks; shrank sidebar from 192 to 160 px

--- header.php
+++ header.php
@@ -1,12 +1,15 @@
 	<div id="container_header">
 		<div id="header">
 		<div id="header_sub">
-			<h1><a href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>">Bruno Li</a></h1>
+			<h1><a href="<?php echo "http://" . $_SERVER['SERVER_NAME']; ?>">Bruno Li</a></h1>
 			<p>
+			<a href="http://pocketgems.com/about/team/">Sushi Eater</a>
+			at
+			<a href="http://pocketgems.com/">Pocket Gems</a>
+			&bull;
 			<a href="http://www.utexas.edu/">UT</a>
 			<a href="http://www.cs.utexas.edu/">CS</a>
-			<a href="http://www.cs.utexas.edu/~turing/">Turing Scholar</a>
-			&bull;
+			<a href="http://www.cs.utexas.edu/~turing/">Turing Scholar</a>,
 			Class of 2012
 			</p>
 		</div> <!--end header_sub-->
--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 		<div id="sidebar">
 
-			<div id="coding">
-				<h2>coding</h2>
+			<div id="sidebar_first">
+				<h2><img src="img/flagicon-code.png">coding</h2>
 				<ul>
 					<li><a href="umbrella.html">Physics Engine</a></li>
 					<li><a href="fluid.html">2D Fluid</a></li>
@@ -9,25 +9,24 @@
 				</ul>
 			</div>
 
-			<div id="artwork">
-				<h2>artwork</h2>
+			<div id="sidebar_second">
+				<h2><img src="img/flagicon-art.png">artwork</h2>
 				<ul>
 					<li><a href="sketch.html">Sketches</a></li>
-					<li><a href="designs.html">Website Designs</a></li>
 					<li><a href="vector.html">2D Vector</a></li>
 					<li><a href="3d.html">3D Modeling</a></li>
 				</ul>
 			</div>
 
-			<div id="info">
-				<h2>info</h2>
+			<div id="sidebar_third">
+				<h2><img src="img/flagicon-info.png">info</h2>
 				<ul>
 					<li><a href="index.html">About</a></li>
 				</ul>
 			</div>
 
-			<div id="fourth">
-				<h2>&nbsp</h2>
+			<div id="sidebar_fourth">
+				<h2>&nbsp;</h2>
 			</div>
 
 		</div> <!--end sidebar-->

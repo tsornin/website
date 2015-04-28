3e0d2741770cd10da46b5c26e83992a32c3f5b8e
Bruno Li (Desktop) <bruno.sie.li@gmail.com>
2015-04-28 02:07:09 -0700

    move script to just before close body tag

--- postcontent.php
+++ postcontent.php
@@ -11,5 +11,16 @@
 
 </div> <!--end container_all-->
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript"></script>
+<script>
+$(document).ready(function(){
+	$(".slide_content").hide();
+	$(".slide_header").click(function(){
+		$(this).next().slideToggle("fast", "linear");
+		$(this).children().first().html(($(this).children().first().html() == "[-]") ? "[+]" : "[-]");
+	});
+});
+</script>
+
 </body>
 </html>
--- precontent.php
+++ precontent.php
@@ -6,17 +6,6 @@
 <?php include("head.php"); ?>
 <body>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript"></script>
-<script>
-$(document).ready(function(){
-	$(".slide_content").hide();
-	$(".slide_header").click(function(){
-		$(this).next().slideToggle("fast", "linear");
-		$(this).children().first().html(($(this).children().first().html() == "[-]") ? "[+]" : "[-]");
-	});
-});
-</script>
-
 <div id="container_all">
 
 <?php include("header.php"); ?>

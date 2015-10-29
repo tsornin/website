65895191724414150c000dcb50b9ffd094e2b182
Bruno Li (Desktop) <bruno.sie.li@gmail.com>
2015-10-28 23:42:41 -0700

    changed sidebar links to single words

--- sidebar.php
+++ sidebar.php
@@ -6,19 +6,19 @@ function rooturl( $path ) {
 		<div id="sidebar">
 
 			<div id="sidebar_first">
-				<h2><img src="<?=rooturl( "img/flagicon-code.png" ); ?>">coding</h2>
+				<h2><img src="<?=rooturl( "img/flagicon-code.png" ); ?>">code</h2>
 				<ul>
-					<li><a href="<?=rooturl( "thesis.html" ); ?>">Honors Thesis</a></li>
-					<li><a href="<?=rooturl( "umbrella.html" ); ?>">Physics Engine</a></li>
-					<!-- <li><a href="<?=rooturl( "fluid.html" ); ?>">2D Fluid</a></li> -->
+					<li><a href="<?=rooturl( "thesis.html" ); ?>">Thesis</a></li>
+					<li><a href="<?=rooturl( "umbrella.html" ); ?>">Physics</a></li>
+					<!-- <li><a href="<?=rooturl( "fluid.html" ); ?>">Fluid</a></li> -->
 				</ul>
 			</div>
 
 			<div id="sidebar_second">
-				<h2><img src="<?=rooturl( "img/flagicon-art.png" ); ?>">artwork</h2>
+				<h2><img src="<?=rooturl( "img/flagicon-art.png" ); ?>">art</h2>
 				<ul>
-					<li><a href="<?=rooturl( "art2d.html" ); ?>">2D</a></li>
-					<li><a href="<?=rooturl( "art3d.html" ); ?>">3D</a></li>
+					<li><a href="<?=rooturl( "art2d.html" ); ?>">Design</a></li>
+					<li><a href="<?=rooturl( "art3d.html" ); ?>">Modeling</a></li>
 				</ul>
 			</div>
 

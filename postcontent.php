b78ed0af01b923c72dd75640f73dbcf24d430156
Bruno Li (Desktop) <bruno.sie.li@gmail.com>
2016-04-06 04:10:48 -0700

    april 2016: revamped home page, added wiggling app icons, moved portrait to sidebar, added sidebar links. fixed weird bug with 65279 BOM character in chrome.

--- postcontent.php
+++ postcontent.php
@@ -11,16 +11,5 @@
 
 </div> <!--end container_all-->
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript"></script>
-<script>
-$(document).ready(function(){
-	$(".slide_content").hide();
-	$(".slide_header").click(function(){
-		$(this).next().slideToggle("fast", "linear");
-		$(this).children().first().html(($(this).children().first().html() == "[-]") ? "[+]" : "[-]");
-	});
-});
-</script>
-
 </body>
 </html>
--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,8 @@ function rooturl( $path ) {
 			<div id="sidebar_first">
 				<h2><img src="<?=rooturl( "img/flagicon-code.png" ); ?>">code</h2>
 				<ul>
-					<li><a href="<?=rooturl( "thesis.html" ); ?>">Thesis</a></li>
 					<li><a href="<?=rooturl( "umbrella.html" ); ?>">Physics</a></li>
+					<li><a href="<?=rooturl( "thesis.html" ); ?>">Thesis</a></li>
 					<!-- <li><a href="<?=rooturl( "fluid.html" ); ?>">Fluid</a></li> -->
 				</ul>
 			</div>
@@ -18,19 +18,38 @@ function rooturl( $path ) {
 				<h2><img src="<?=rooturl( "img/flagicon-art.png" ); ?>">art</h2>
 				<ul>
 					<li><a href="<?=rooturl( "art2d.html" ); ?>">Design</a></li>
-					<li><a href="<?=rooturl( "art3d.html" ); ?>">Modeling</a></li>
+					<!-- <li><a href="<?=rooturl( "art3d.html" ); ?>">Modeling</a></li> -->
 				</ul>
 			</div>
-
+<!-- 
 			<div id="sidebar_third">
 				<h2><img src="<?=rooturl( "img/flagicon-info.png" ); ?>">info</h2>
 				<ul>
 					<li><a href="<?=rooturl( "index.html" ); ?>">About</a></li>
 				</ul>
 			</div>
-
+ -->
 			<div id="sidebar_fourth">
 				<h2>&nbsp;</h2>
+			</div>
+
+			<div id="sidebar_portrait">
+			<div id="portrait">
+				<img src="img/avatar_scarfa.png" alt="Picture of Bruno">
+			</div>
+			</div>
+
+			<div id="sidebar_blogroll">
+			<div id="sidebar_blogroll_title">
+				<span class="small_caps">Stuff I'm reading:</span>
+			</div>
+			<div id="sidebar_blogroll_links">
+				<a href="http://xkcd.com/">xkcd</a>
+				<a href="http://slatestarcodex.com/">Slate Star Codex</a>
+				<a href="http://www.theeffectiveengineer.com/">The Effective Engineer</a>
+				<a href="http://putthison.com/">Put This On</a>
+				<a href="https://www.reddit.com/r/fitness">r/fitness</a>
+			</div>
 			</div>
 
 		</div> <!--end sidebar-->

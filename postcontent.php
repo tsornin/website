c9c3bc093ef15687bb58479fa40f1d8c746a3fb4
Bruno Li (Desktop) <bruno.sie.li@gmail.com>
2016-04-06 04:10:48 -0700

    april 2016: removed 3d page, revamped home page, added wiggling icons. hot stuff. WIP

--- postcontent.php
+++ postcontent.php
@@ -12,12 +12,31 @@
 </div> <!--end container_all-->
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript"></script>
+<script src="js/jquery.classywiggle.js" type="text/javascript"></script>
 <script>
-$(document).ready(function(){
-	$(".slide_content").hide();
-	$(".slide_header").click(function(){
-		$(this).next().slideToggle("fast", "linear");
-		$(this).children().first().html(($(this).children().first().html() == "[-]") ? "[+]" : "[-]");
+$( document ).ready( function() {
+	$( ".slide_content" ).hide();
+	$( ".slide_header" ).click( function() {
+		$( this ).next().slideToggle( "fast", "linear" );
+		$( this ).children().first().html( ($( this ).children().first().html() == "[-]" ) ? "[+]" : "[-]" );
+	});
+
+	$( "div.appicon_desc" ).hide();
+	$( "div#app0_desc" ).show();
+	$( "span.appicon" ).addClass( "mouseover" );
+	$( "span.appicon" ).bind( "mouseover", function() {
+		var desc_id = "#" + $( this ).attr( "id" ) + "_desc";
+		var was_visible = ( $( desc_id ).is( ":visible" ) );
+		$( "div.appicon_desc" ).stop().hide();
+		$( desc_id ).stop().fadeIn( was_visible ? 0 : 400 );
+
+		var id = "#" + $( this ).attr( "id" );
+		$( "span.appicon" ).addClass( "mouseover" );
+		// $( ".appicon" ).find( "img" ).ClassyWiggle( "stop" );
+		$( id ).removeClass( "mouseover" );
+		if ( ! $( id ).find( "img" ).ClassyWiggle( "isWiggling" ) ) {
+			$( id ).find( "img" ).ClassyWiggle( "start", { "limit" : "6" } );
+		}
 	});
 });
 </script>
--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,8 @@ function rooturl( $path ) {
 			<div id="sidebar_first">
 				<h2><img src="<?=rooturl( "img/flagicon-code.png" ); ?>">code</h2>
 				<ul>
-					<li><a href="<?=rooturl( "thesis.html" ); ?>">Thesis</a></li>
 					<li><a href="<?=rooturl( "umbrella.html" ); ?>">Physics</a></li>
+					<li><a href="<?=rooturl( "thesis.html" ); ?>">Thesis</a></li>
 					<!-- <li><a href="<?=rooturl( "fluid.html" ); ?>">Fluid</a></li> -->
 				</ul>
 			</div>
@@ -18,19 +18,23 @@ function rooturl( $path ) {
 				<h2><img src="<?=rooturl( "img/flagicon-art.png" ); ?>">art</h2>
 				<ul>
 					<li><a href="<?=rooturl( "art2d.html" ); ?>">Design</a></li>
-					<li><a href="<?=rooturl( "art3d.html" ); ?>">Modeling</a></li>
+					<!-- <li><a href="<?=rooturl( "art3d.html" ); ?>">Modeling</a></li> -->
 				</ul>
 			</div>
-
+<!-- 
 			<div id="sidebar_third">
 				<h2><img src="<?=rooturl( "img/flagicon-info.png" ); ?>">info</h2>
 				<ul>
 					<li><a href="<?=rooturl( "index.html" ); ?>">About</a></li>
 				</ul>
 			</div>
-
+ -->
 			<div id="sidebar_fourth">
 				<h2>&nbsp;</h2>
+			</div>
+
+			<div id="portrait">
+				<img src="img/avatar_scarfa.png" alt="Picture of Bruno">
 			</div>
 
 		</div> <!--end sidebar-->

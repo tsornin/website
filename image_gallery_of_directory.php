4442bce5edda9cc62b0070dfe49240f5cadd1682
Bruno Li <bruno.sie.li@gmail.com>
2020-01-29 23:21:38 -0800

    fix image gallery to use thumb directory

--- image_gallery_of_directory.php
+++ image_gallery_of_directory.php
@@ -5,7 +5,7 @@ function image_gallery_of_directory( $dir ) {
 	sort( $images, SORT_NATURAL );
 	foreach ( $images as $file ) {
 		$basename = basename( $file );
-		echo "\n" . '<a href="' . $dir . '/' . $basename . '"><img src="' . $dir . '-thumb/' . $basename . '" /></a>';
+		echo "\n" . '<a href="' . $dir . '/' . $basename . '"><img src="' . $dir . '/thumb/' . $basename . '" /></a>';
 	}
 	echo '</div>';
 }

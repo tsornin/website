467bfa1323406598c01a9ee2adeac53b1d28f62e
Bruno Li <bruno@pocketgems.com>
2025-06-08 22:24:43 -0700

    update google ajax link to https

--- precontent.php
+++ precontent.php
@@ -6,7 +6,7 @@
 <?php include("head.php"); ?>
 <body>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript"></script>
 <script>
 $( document ).ready( function() {
 	$( ".slide_content" ).hide();
